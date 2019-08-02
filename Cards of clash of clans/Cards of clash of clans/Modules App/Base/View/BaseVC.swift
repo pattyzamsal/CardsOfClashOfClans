d0505a9e749fb4746b6f3a3149ae6e066d0149ca
Patricia Zambrano <pazs1991@gmail.com>
2019-08-02 01:28:37 -0500

    Create base MVP

---
+++ Cards of clash of clans/Cards of clash of clans/Modules App/Base/View/BaseVC.swift
@@ -0,0 +1,19 @@
+//
+//  BaseVC.swift
+//  Cards of clash of clans
+//
+//  Created by Patricia Zambrano on 8/1/19.
+//  Copyright © 2019 Patricia Zambrano. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
