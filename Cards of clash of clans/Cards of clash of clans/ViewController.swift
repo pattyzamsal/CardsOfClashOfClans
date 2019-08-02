f6d3eeaf224ca5ed384141c7a83920e260a1c6a2
Patricia Zambrano <pazs1991@gmail.com>
2019-08-01 23:18:48 -0500

    Create project with the template of single view

---
+++ Cards of clash of clans/Cards of clash of clans/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cards of clash of clans
+//
+//  Created by Patricia Zambrano on 8/1/19.
+//  Copyright © 2019 Patricia Zambrano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
