23835177afa5b0790fd47d07e54f718cf3b4e6da
Patricia Zambrano <pazs1991@gmail.com>
2019-08-02 01:30:35 -0500

    Create basic cell to manage the information inside the table view

---
+++ Cards of clash of clans/Cards of clash of clans/Modules App/List Cards/View/Cell/CardCell.swift
@@ -0,0 +1,24 @@
+//
+//  CardCell.swift
+//  Cards of clash of clans
+//
+//  Created by Patricia Zambrano on 8/2/19.
+//  Copyright © 2019 Patricia Zambrano. All rights reserved.
+//
+
+import UIKit
+
+class CardCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
