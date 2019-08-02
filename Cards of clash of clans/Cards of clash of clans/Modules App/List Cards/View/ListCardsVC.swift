7e2c6b066ea95d0c787187a641238e1c3628bb37
Patricia Zambrano <pazs1991@gmail.com>
2019-08-02 01:31:09 -0500

    Create table view to show all cards

---
+++ Cards of clash of clans/Cards of clash of clans/Modules App/List Cards/View/ListCardsVC.swift
@@ -0,0 +1,30 @@
+//
+//  ListCardsVC.swift
+//  Cards of clash of clans
+//
+//  Created by Patricia Zambrano on 8/2/19.
+//  Copyright © 2019 Patricia Zambrano. All rights reserved.
+//
+
+import UIKit
+
+class ListCardsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
