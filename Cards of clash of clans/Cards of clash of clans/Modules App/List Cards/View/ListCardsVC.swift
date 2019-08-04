4fc409d91fee61e07187662cee9c5365365af5c9
Patricia Zambrano <pazs1991@gmail.com>
2019-08-03 19:31:00 -0500

    Create functions to show the table view with data

--- Cards of clash of clans/Cards of clash of clans/Modules App/List Cards/View/ListCardsVC.swift
+++ Cards of clash of clans/Cards of clash of clans/Modules App/List Cards/View/ListCardsVC.swift
@@ -10,21 +10,54 @@ import UIKit
 
 class ListCardsVC: UIViewController {
 
+    @IBOutlet weak var tvListCards: UITableView!
+    
+    private var listOfCards: [(name: String, type: String)] = [("Arrows", "Spell"), ("Bomber", "Troop"), ("Archers", "Troop")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "List of cards"
+        self.navigationItem.backBarButtonItem?.title = ""
+        
+        tvListCards.delegate = self
+        tvListCards.dataSource = self
+        tvListCards.separatorStyle = .none
+        let nib = UINib(nibName: "CardCell", bundle: nil)
+        tvListCards.register(nib, forCellReuseIdentifier: "cardCell")
+        
+        orderCards()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func orderCards() {
+        listOfCards = listOfCards.sorted(by: { $0.name < $1.name })
+        tvListCards.reloadData()
     }
-    */
+}
 
+extension ListCardsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listOfCards.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tvListCards.dequeueReusableCell(withIdentifier: "cardCell") as! CardCell
+        
+        if !listOfCards.isEmpty {
+            cell.setCardCell(name: listOfCards[indexPath.row].name, type: listOfCards[indexPath.row].type)
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let detailCardVC = DetailCardVC()
+        detailCardVC.specificCard = listOfCards[indexPath.row].name
+        self.navigationController?.pushViewController(detailCardVC, animated: false)
+    }
 }
